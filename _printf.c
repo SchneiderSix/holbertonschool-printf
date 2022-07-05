9f1f624d146df2bbd04c951ff8b56d84fd9d5b1e
SchneiderSix <knuckles116635@gmail.com>
2022-07-05 14:37:52 -0700

    test

---
+++ _printf.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	va_list parameters;
+	int = 0;
+	char *p;
+
+	va_start(parameters, format);
+	while (format[i])
+	{
+		if(format[i] = %)
+		{
+
+		switch (format[i])
+		{
+			case '
+	}
