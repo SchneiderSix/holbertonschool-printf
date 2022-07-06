ce65fbd44b65cccead1713488445c4a7bdc13941
Mateo Bonino <5077@holbertonschool.com>
2022-07-06 16:28:28 -0400

    switch

--- functions.c
+++ functions.c
@@ -1,12 +1,22 @@
 #include "main.h"
-
+/**
+* print_char - prints a char
+* @a: the char
+*Return: j
+*/
 int print_char(char a)
 {
 	int j = 1;
+
 	write(1, &a, 1);
 	return (j);
 }
-
+/**
+* print_string - prints a string
+* @s: the string
+*
+*Return: the length of the string
+*/
 int print_string(char *s)
 {
 	int len;
@@ -29,11 +39,16 @@ int print_string(char *s)
 	}
 	return (len);
 }
-
+/**
+* just_in_case - checks format[i + 1]
+* @a: format[i + 1]
+*
+*Return: the amount of chars printed
+*/
 int just_in_case(char a)
 {
 	int b = 1;
-	
+
 	if (a != '%')
 	{
 		write(1, "%", 1);
