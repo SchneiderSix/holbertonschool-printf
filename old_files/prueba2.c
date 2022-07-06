7b218a600604838dd17dd3c94b49578a2a09a943
Mateo Bonino <5077@holbertonschool.com>
2022-07-06 15:36:01 -0400

    switch

--- old_files/prueba2.c
+++ old_files/prueba2.c
@@ -1,32 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: 0 on success, error code otherwise
- */
 int main(void)
 {
+	/*int emptychar = 2;*/
+	/*int len = _printf("%c", 'H'); test case 1
+	int len2 = printf("%c", 'H');*/
+
+	/*int len = _printf("%s", "String"); test case 2
+	int len2 = printf("%s", "String");*/
+
+	/*int len = _printf("%%"); test case 3
+	int len2= printf("%%");*/
+
+	/*int len = _printf("%!");
+	int len2= printf("%!");
+	int len2 = printf("%");*/
+
+	/*if (len != len2)
+	{
+		printf("Length differ!\n");
+		printf("len: %d", len);
+		printf("\nlen2: %d", len2);
+		return (1);
+	}
+	printf("Equal lengths\n");
+
+	if (len != len2)
+	{
+		printf("Length differ!\n");
+		printf("len: %d\n", len);
+		printf("len2: %d\n", len2);
+		return (1);
+	}
+	printf("Equal length\n");
+	return (0);*/
+
 	/*int len, len2;
 
-	len = _printf("%c", 'S');
-	len2 = printf("%c", 'S');
+	len = _printf("%!\n");
+	len2 = printf("%!\n");
 	fflush(stdout);
 	if (len != len2)
 	{
-        printf("\nlen1: %d\nlen2: %d\n", len, len2);
 		printf("Lengths differ.\n");
 		fflush(stdout);
 		return (1);
-	}
-	printf("\nlen1: %d\nlen2: %d\n", len, len2);
-	_printf("%s", "Hola");
-	s_printf("%c %s Hola\n", 'H', "Hola");
-	_printf("%%\n");
-	_printf("%a\n");
-	_printf("%");*/
-	s_printf("%c %s Hola", 'H', "Hola");
+	}*/
+	_printf(NULL);
 	return (0);
 }
