65768851fcd480775ea321374e1550fbd25a911f
Mateo Bonino <5077@holbertonschool.com>
2022-07-06 16:11:55 -0400

    switch

--- _printf.c
+++ _printf.c
@@ -15,21 +15,22 @@ int _printf(const char *format, ...)
 		{
 			if (format[i] == '%')
 			{
-				i++;
-				switch(format[i])
+				switch(format[i + 1])
 				{
 					case 'c':
 						ch = va_arg(parameters, int);
 						print_char(ch);
+						i + = 2;
 						len++;
 						break;
 					case 's':
 						len += print_string(va_arg(parameters, char *));
+						i += 2;
 						break;
 					case '\0':
 						return (-1);
 					default:
-						len += just_in_case(format[i]);
+						len += just_in_case(format[i + 1]);
 						break;
 				}
 			}
