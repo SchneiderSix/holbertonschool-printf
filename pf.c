1d329daadcfcb306e171b1ace69d90b0ed84f442
Mateo Bonino <5077@holbertonschool.com>
2022-07-09 00:13:47 -0400

    Works (again)

--- pf.c
+++ pf.c
@@ -1,61 +1,53 @@
 #include "main.h"
-
+/**
+* _printf - Copy of the original printf() C Function
+* @format: the parameters to print
+*Return: the amount of chars printed :)
+*/
 int _printf(const char *format, ...)
 {
-  va_list parameters;
-  int i = 0, j=0;
-  va_start(parameters, format);
-  while (format && format[i])
-  {
-    if(format[i] == '%'){
-      i++;
-      switch (format[i]) {
-        /* Convert char */
-        case 'c': {
-		print_char(va_arg(parameters, int));
-		j++;
-          	break;
-        }
-        /* Convert decimal */
-        case 'd':
-		j += print_per_d(va_arg(parameters, int));
-        	break;
-	case 'i': {
-		j += print_per_d(va_arg(parameters, int));
-		break;
-	case '%':
-		print_char('%');
-		j++;
-		break;
-	case '\0':
-		break;
-	}
-        /* Convert hex */
-        case 'x': {
-          /*_itoa(va_arg( vl, int ), tmp, 16);*/
-          /*strcpy(&buff[j], tmp);*/
-          /*j += strlen(tmp);*/
-          break;
-        }
-        /* Convert octal */
-        case 'o': {
-          /*_itoa(va_arg( vl, int ), tmp, 8);*/
-          /*strcpy(&buff[j], tmp);*/
-          /*j += strlen(tmp);*/
-          break;
-        }
-        /* copy string */
-        case 's': {
-          j += print_string(va_arg(parameters, char *));
-          break;
-        }
-      }
-    } else {
-	write(1, &format[i], 1);
-      	j++;
-    }
-    i++;
-  }
+	va_list parameters;
+	int i = 0, j=0;
+
+	va_start(parameters, format);
+	while (format && format[i])
+	{
+		if(format[i] == '%')
+		{
+			i++;
+      			switch (format[i]) 
+			{
+        			case 'c':
+					print_char(va_arg(parameters, int));
+					j++;
+          				break;
+				/*case 'd':
+					j += print_per_d(va_arg(parameters, int));
+        				break;
+				case 'i':
+					j += print_per_d(va_arg(parameters, int));
+					break;*/
+				case 's':
+					j += print_string(va_arg(parameters, char *));
+					break;
+				case '%':
+					print_char('%');
+					j++;
+					break;
+				case '\0':
+					return (-1);
+				default:
+					j += just_in_case(va_arg(parameters, int));
+					break;
+			}
+        	}
+    		else
+		{
+			write(1, &format[i], 1);
+      			j++;
+		}
+		i++;
+  	}
   va_end(parameters);
   return (j);
 }
