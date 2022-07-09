8edd7bcedad854deecf948e0bbb453518bcdd6fd
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-07-09 18:07:10 -0300

    Update main.h

--- main.h
+++ main.h
@@ -8,22 +8,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*char *_strrev (char *str);*/
-/*char *_itoa(int i, int base);*/
 int print_per_d(int n);
 int _printf(const char *format, ...);
-int print_int(int num);
-int num_len(int n);
-void print_n(int n);
 void print_char(char a);
 int print_string(char *s);
 int just_in_case(char a);
-void pi(int num);
-/*char *my_itoa(int n, char *buff, int base);*/
-/*int my_itoa(int val, char *buff);*/
-/*char *_strcpy(char *dest, char *src);*/
-int _strle(char *s);
-/*char *s_itoa(int num, int base);*/
-/*void rev_str(char *s);*/
 
 #endif /*MAIN_H*/
