9ddb7672cc18b81bc7670bfb3f02a1076e90e44a
Mateo Bonino <5077@holbertonschool.com>
2022-07-09 00:35:58 -0400

    idk if it works

--- pruebas/main.c
+++ pruebas/main.c
@@ -2,8 +2,11 @@
 
 int main(void)
 {
-	print("%d %s %c\n", INT_MIN, "Funciona", 'A');
-	print("Funciona!!\n");
-	print("Hola\n");
+	int funciona = 100;
+	_printf("%d %s %c %d\n", INT_MIN, "Funciona", 'A', 1000);
+	_printf("Funciona!!\n");
+	_printf("Hola\n");
+	_printf("%d %d\n", 2000000, -1556);
+	_printf("%i\n", funciona);
 	return (0);
 }
